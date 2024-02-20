98b12d31f5975b92cc06f6aee217b9b7edcdc0d6
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-20 12:53:38 +0100

    Add student to cohort

--- spec/cohort.spec.js
+++ spec/cohort.spec.js
@@ -1,4 +1,4 @@
-const { a, b } = require('../src/CohortManager.js')
+const { a, b, c, d } = require('../src/CohortManager.js')
 
 describe('Cohort basic testing', () => {
   it('Cohort should exist', () => {
@@ -8,15 +8,48 @@ describe('Cohort basic testing', () => {
   })
 
   it('Cohort should have a name', () => {
-    expect(a.name).toBeDefined()
+    const res = a('TestCohort')
+    expect(res.name).toBeDefined()
   })
 
   it('Find cohorts', () => {
+    a('TestCohort')
     const res = b('TestCohort')
     expect(res).toBeDefined()
   })
 
   it('Attempt to find undefined cohort', () => {
     expect(() => b('UndefinedCohort')).toThrowError()
+  })
+
+  it('Generate student', () => {
+    const student = d(
+      'la8pv',
+      'Tim',
+      'Johnsen',
+      'tim.johnsen',
+      'tim.johnsen@mail.com'
+    )
+    expect(student.studentID).toEqual('la8pv')
+    expect(student.firstName).toEqual('Tim')
+    expect(student.lastName).toEqual('Johnsen')
+    expect(student.githubName).toEqual('tim.johnsen')
+    expect(student.email).toEqual('tim.johnsen@mail.com')
+  })
+
+  it('Add student to cohort', () => {
+    const cohort = a('CohortWithStudent')
+    const preRes = cohort
+    expect(preRes.students.length).toEqual(0)
+    const student = d(
+      'la8pv',
+      'Tim',
+      'Johnsen',
+      'tim.johnsen',
+      'tim.johnsen@mail.com'
+    )
+    c(student, 'CohortWithStudent')
+    const postRes = b('CohortWithStudent')
+    expect(postRes.students.length).toEqual(1)
   })
 })
--- src/CohortManager.js
+++ src/CohortManager.js
@@ -2,7 +2,8 @@ const cohorts = []
 
 function generateCohort(cohortName) {
   const cohort = {
-    name: cohortName
+    name: cohortName,
+    students: []
   }
 
   cohorts.push(cohort)
@@ -10,14 +11,32 @@ function generateCohort(cohortName) {
 }
 
 function searchForCohort(cohortName) {
-  const cohort = cohorts.filter((cohort) => cohort.name === cohortName)
-  if (cohort.length === 0) {
+  const cohort = cohorts.find((cohort) => cohort.name === cohortName)
+  if (!cohort) {
     throw new Error('No cohort of the provided name could be found!')
   }
   return cohort
 }
 
+function addStudentToCohort(student, cohortName) {
+  const cohort = searchForCohort(cohortName)
+  cohort.students.push(student)
+  return cohort
+}
+
+function generateStudentObject(id, firstName, lastName, githubUsername, email) {
+  const student = {}
+  student.studentID = id
+  student.firstName = firstName
+  student.lastName = lastName
+  student.githubName = githubUsername
+  student.email = email
+  return student
+}
+
 module.exports = {
   a: generateCohort,
-  b: searchForCohort
+  b: searchForCohort,
+  c: addStudentToCohort,
+  d: generateStudentObject
 }
